360895e619b9e80e8cd09f103ec5147c256f3023
Martin Broerse <broerse@martinic.com>
2015-04-13 23:51:31 +0200

    Add posts-list component

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,7 +1,11 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
- model: function(params) {
+  model: function(params) {
    return this.modelFor('posts').findBy('id', params.site_id);
- }
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('posts', this.modelFor('posts'));
+  }
 });
